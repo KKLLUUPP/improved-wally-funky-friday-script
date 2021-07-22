af3883398f155103ff8090274af163217542c166
KKLLUUPP <87819300+KKLLUUPP@users.noreply.github.com>
2021-07-22 09:39:51 -0500

    Add files via upload

---
+++ jdnfjd.lua
@@ -0,0 +1,4 @@
+if game.Placeid -- 155615604 then
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Prison Life Hack Gui", "Synapse")
+end
